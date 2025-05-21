898c844940c56eb47dbeb0267f85a070e9aa09f7
girish.jangam <59350195+Girish-Jangam@users.noreply.github.com>
2025-05-22 01:55:56 +0530

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { loginAdmin, registerAdmin } = require('../../Backend/services/admin');
+const { loginAdmin, registerAdmin } = require('../services/admin');
 const {
   getDestinationGuides,
   getTripItineraries,
